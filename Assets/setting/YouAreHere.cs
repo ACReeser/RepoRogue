aee547de9174685b5a0b924590481e64ac5019fc
ACReeser <rogueconformist@gmail.com>
2018-11-10 23:28:28 -0600

    Minimap implementation

---
+++ Assets/setting/YouAreHere.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YouAreHere : MonoBehaviour {
+    public RectTransform YouAreHereSprite;
+    public Transform Player;
+
+    private Vector2 newPosition = Vector2.zero;
+
+	// Use this for initialization
+	void Start () {
+        //StartCoroutine(Follow());
+	}
+
+    private float time;
+    void Update()
+    {
+        //YouAreHereSprite.anchoredPosition = Vector2.Lerp(YouAreHereSprite.anchoredPosition, newPosition, Time.deltaTime);
+        //time += Time.deltaTime;
+        YouAreHereSprite.anchoredPosition  = new Vector2(Player.position.x / 69.8f * 100, Player.position.z / 69.8f * 100);
+    }
+
+    private IEnumerator Follow()
+    {
+        while (isActiveAndEnabled)
+        {
+            newPosition = new Vector2(Player.position.x / 69.8f * 100, Player.position.z / 69.8f * 100);
+            time = 0;
+            YouAreHereSprite.SetAsLastSibling();
+            yield return new WaitForSeconds(.5f);
+        }
+    }
+    
+}

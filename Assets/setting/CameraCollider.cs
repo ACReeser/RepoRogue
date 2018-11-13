0b9133545656cb923e369046c7371b898a8d46af
ACReeser <rogueconformist@gmail.com>
2018-11-12 22:38:27 -0600

    Disappearing clipping buildings

---
+++ Assets/setting/CameraCollider.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCollider : MonoBehaviour {
+    private Dictionary<int, MeshRenderer> hiddenBuildings = new Dictionary<int, MeshRenderer>();
+    public void OnTriggerEnter(Collider collider)
+    {
+        if (collider != null &&
+            collider.transform.childCount > 0 &&
+            collider.transform.GetChild(0).CompareTag("invisible_fence") &&
+            !hiddenBuildings.ContainsKey(collider.GetInstanceID()))
+        {
+            var buildingRenderer = collider.GetComponent<MeshRenderer>();
+            hiddenBuildings.Add(collider.GetInstanceID(), buildingRenderer);
+            buildingRenderer.enabled = false;
+            collider.transform.GetChild(0).GetComponent<MeshRenderer>().enabled = true;
+            //buildingRenderer.gameObject.isStatic = false;
+        }            
+    }
+
+    public void OnTriggerExit(Collider collider)
+    {
+        if (collider != null &&
+            hiddenBuildings.ContainsKey(collider.GetInstanceID()))
+        {
+            var buildingRenderer = collider.GetComponent<MeshRenderer>();
+            hiddenBuildings.Remove(collider.GetInstanceID());
+            buildingRenderer.enabled = true;
+            collider.transform.GetChild(0).GetComponent<MeshRenderer>().enabled = false;
+        }
+    }
+}
